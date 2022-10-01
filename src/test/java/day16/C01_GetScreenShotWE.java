189ef16c7362a9d71e767325ba8e20723813c0c9
Nida Yucedal <yucedalnida@gmail.com>
2022-10-01 17:25:55 +0300

    com

--- src/test/java/day16/C01_GetScreenShotWE.java
+++ src/test/java/day16/C01_GetScreenShotWE.java
@@ -16,14 +16,18 @@ public class C01_GetScreenShotWE extends TestBaseBeforeAfter {
     public void test1() throws IOException {
         //Amazon sayfasına gidelim
         driver.get("https://amazon.com");
+
         //Nutelle aratalım
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Nutella", Keys.ENTER);
+
         //Arama sonuc yazısının resmini alalım
         WebElement aramaSonucu = driver.findElement
                 (By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']"));
         File nutellaAramaSonucu = aramaSonucu.getScreenshotAs(OutputType.FILE);
+
         //Sadece Web elementin resmini alacaksak TakeScreenShot classını kullanmama gerek yok
         //Locate ettiğimiz webelementi direk geçici bir file'a atıp FileUtils ile kopyalayıp yolunu(path) belirtiriz
+
         FileUtils.copyFile(nutellaAramaSonucu, new File("target/ekranGoruntusuWE/WebESS" + tarih + ".jpeg"));
     }
 }
